3b8fc4dd226ae65fadf0a3d13abec51c4d736c7e
cjm <1137532701@qq.com>
2020-07-23 13:43:58 +0800

    12.网络请求和封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // pages/home/home.js
+import request from '../../service/network'
 Page({
 
   /**
@@ -12,55 +13,52 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //1.原生的方式发送网络请求
+    this.get_data_origin()
+    //2.使用封装的request发送网络请求
+    request({
+      url: 'http://123.207.32.32:8000/recommend',
+    }).then(res=>{
+      console.log(res)
+    }).catch(err=>{
+      console.log(err)
+    })
+  } ,
+  get_data_origin(){
+    //发送网络请求
+    //1.发送最简单的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success: (res)=>{
+    //     console.log(res)
+    //   }
+    // })
+    //2.get请求，并且携带参数
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   data: {
+    //     type: 'sell',
+    //     page: 1
+    //   },
+    //   success: (res)=>{
+    //     console.log(res)
+    //   }
+    // })
+    //3.post请求，并且携带参数
+  //   wx.request({
+  //     url: 'http://httpbin.org/post',
+  //     method: 'post',
+  //     data: {
+  //       name: 'chenjiemin',
+  //       age: 18
+  //     },
+  //     success: (res)=>{
+  //       console.log(res)
+  //     },
+  //     fail: err=>{
+  //       console.log(err)
+  //     }
+  //   })
+  // }
   }
 })
